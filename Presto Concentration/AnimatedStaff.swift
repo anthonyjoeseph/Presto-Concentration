0dba6056b250fe5c267357f1dca7896aaa9bbfaa
anthonygabriele <anthonyjoeseph@gmail.com>
2015-08-26 10:24:25 -0400

    starting out with everything

---
+++ Presto Concentration/AnimatedStaff.swift
@@ -0,0 +1,23 @@
+//
+//  AnimatedStaff.swift
+//  Presto Concentration
+//
+//  Created by Anthony Gabriele on 8/26/15.
+//  Copyright (c) 2015 Anthony Gabriele. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class AnimatedStaff: SKScene {
+    let player = SKSpriteNode(imageNamed: "player")
+    
+    override func didMoveToView(view: SKView) {
+        // 2
+        backgroundColor = SKColor.whiteColor()
+        // 3
+        player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
+        // 4
+        addChild(player)
+    }
+}
---
+++ Presto Concentration/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Presto Concentration
+//
+//  Created by Anthony Gabriele on 8/19/15.
+//  Copyright (c) 2015 Anthony Gabriele. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var skViewOp:SKView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let skView = skViewOp!
+        let scene = AnimatedStaff(size: skView.bounds.size)
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .ResizeFill
+        skView.presentScene(scene)
+    }
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+
+}
+
